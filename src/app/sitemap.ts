122ac51255270534913339e77dddc06fab0f9f12
pointvirgulagency <pointvirgulagency2024@gmail.com>
2026-09-04 00:19:42 +0100

    Ajouter les articles au sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,6 +6,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: baseUrl },
     { url: `${baseUrl}/blog` },
+    { url: `${baseUrl}/blog/kinesitherapie-grossesse` },
+    { url: `${baseUrl}/blog/reeducation-sportive` },
+    { url: `${baseUrl}/blog/mal-de-dos-kinesitherapie` },
     { url: `${baseUrl}/contact` },
     { url: `${baseUrl}/le-cabinet` },
     { url: `${baseUrl}/nos-traitements` },
