78b68ab35559fc5c93aa60e542fc87428ef2460b
pointvirgulagency <pointvirgulagency2024@gmail.com>
2026-09-03 10:04:15 +0100

    Ajouter le sitemap du site

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://www.racinesphysio.com";
+
+  return [
+    { url: baseUrl },
+    { url: `${baseUrl}/blog` },
+    { url: `${baseUrl}/contact` },
+    { url: `${baseUrl}/le-cabinet` },
+    { url: `${baseUrl}/nos-traitements` },
+    { url: `${baseUrl}/parcours-patients` },
+  ];
+}
